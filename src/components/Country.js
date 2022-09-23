c3a8780ca98047787e38ec005405ef700973a483
BabriWala <hanzalafarhann@gmail.com>
2022-09-23 18:22:19 +0600

    all done

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './country.css';
 
 const Country = (props) => {
+    // console.log(props.country);
+    const {name, area, flags} = props.country;
     return (
-        <div>
-            <h2>Name: {props.name}</h2>
+        <div className='container'>
+            <img className='flags' src={flags.png} alt="" />
+            <h2>Name: {name.common}</h2>
+            <p>Area: {area}</p>
+            <p>Population: {props.population}</p>
         </div>
     );
 };
--- src/components/LoadCountries.js
+++ src/components/LoadCountries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Country from './Country';
+import './country.css';
 
 const LoadCountries = () => {
     const [countries, setCountries] = useState([]);
@@ -9,14 +10,21 @@ const LoadCountries = () => {
     .then(res => res.json())
     .then(data => setCountries(data))
   },[])
-  // console.log(countries)
+  // console.log(countries[0])
     return (
         <div>
             <h1>Visiting Every Country of The World</h1>
             <h3>Available Countries: {countries.length}</h3>
+            <div className='main-container'>
+            {
+              countries.map(country => <Country key={country.flag} population={country.population
+              } country = {country}></Country>)
+            }
             {/* {
-              countries.map(country => <Country>)
+              countries.map(country => <Country name={country.name.common} key={country.ccn4} area={country.area} population={country.population
+              } country = {country}></Country>)
             } */}
+            </div>
         </div>
     );
 };
