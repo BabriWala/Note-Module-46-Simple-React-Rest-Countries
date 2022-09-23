75141c3c819cdfd246ff8ec002eed28168c0a1ad
BabriWala <hanzalafarhann@gmail.com>
2022-09-23 19:03:50 +0600

    All Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import LoadCountries from './components/LoadCountries';
+import Footers from './components/Footer/Footers';
+import Header from './components/Header/Header';
 
 
 function App() {
   
   return (
     <div className="App">
+      <Header></Header>
       <LoadCountries></LoadCountries>
+      <Footers></Footers>
     </div>
   );
 }
---
+++ src/components/Footer/Footers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footers = () => {
+    return (
+        <div>
+            <p>Copyright Protected.</p>
+        </div>
+    );
+};
+
+export default Footers;
